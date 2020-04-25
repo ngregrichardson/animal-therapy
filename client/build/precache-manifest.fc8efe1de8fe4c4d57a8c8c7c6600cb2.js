25bcdd17d1442857d605a1e26e74670ea2ead8d0
Noah <ngregrichardson@gmail.com>
2020-04-24 20:23:49 -0400

    feat: Add express server

---
+++ client/build/precache-manifest.fc8efe1de8fe4c4d57a8c8c7c6600cb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd99fb700de242bf7204af22de7f9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca6c1e7da9b8b6079116",
+    "url": "/static/css/2.1173412f.chunk.css"
+  },
+  {
+    "revision": "9aff91cb02da96c06f7a",
+    "url": "/static/css/main.03c74552.chunk.css"
+  },
+  {
+    "revision": "ca6c1e7da9b8b6079116",
+    "url": "/static/js/2.891ed5a4.chunk.js"
+  },
+  {
+    "revision": "b7189180dbce6d61f34aa971007229e0",
+    "url": "/static/js/2.891ed5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aff91cb02da96c06f7a",
+    "url": "/static/js/main.6c0cc543.chunk.js"
+  },
+  {
+    "revision": "04a50c549bac22014492",
+    "url": "/static/js/runtime-main.f9362f4a.js"
+  }
+]);
